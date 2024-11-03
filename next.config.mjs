1c80f6c8c500849a6c876e68c0a9463cd3086684
delroscol98 <collindelrosario98@gmail.com>
2024-11-04 09:33:52 +1100

    Streaming UI with suspense: Cabin List

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rusrliojlcyzuxibdewf.supabase.co",
+        port: "",
+        pathname: "/storage/v1/object/public/cabin-images/**",
+      },
+    ],
+  },
+};
 
 export default nextConfig;
