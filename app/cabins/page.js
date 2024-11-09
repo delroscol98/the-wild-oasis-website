8763444247a8f4fcd5ab422a12ca2c0fc08aecf1
delroscol98 <collindelrosario98@gmail.com>
2024-11-09 21:47:54 +1100

    Using the Context API for state management

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -2,6 +2,7 @@ import { Suspense } from "react";
 import CabinsList from "../_components/CabinsList";
 import Spinner from "../_components/Spinner";
 import Filter from "../_components/Filter";
+import ReservationReminder from "../_components/ReservationReminder";
 
 export const revalidate = 3600;
 // export const revalidate = 15;
@@ -32,6 +33,7 @@ export default function CabinsPage({ searchParams }) {
 
       <Suspense fallback={<Spinner />} key={filter}>
         <CabinsList filter={filter} />
+        <ReservationReminder />
       </Suspense>
     </div>
   );
