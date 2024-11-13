88f8018a3dcabbaa2014ea0e287084d8429858ae
delroscol98 <collindelrosario98@gmail.com>
2024-11-13 21:35:21 +1100

    Building the guests reservation page and deleting bookings

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -2,7 +2,11 @@
 
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
-import { updateGuest as updateGuestApi } from "./data-service";
+import {
+  deleteBooking,
+  getBookings,
+  updateGuest as updateGuestApi,
+} from "./data-service";
 
 export async function updateGuest(formData) {
   const session = await auth();
@@ -19,6 +23,21 @@ export async function updateGuest(formData) {
   updateGuestApi(session.user.guestId, updateData);
 
   revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const guestBookings = await getBookings(session.user.guestId);
+  const guestBookingIds = guestBookings.map((booking) => booking.id);
+
+  if (!guestBookingIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+
+  deleteBooking(bookingId);
+
+  revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
