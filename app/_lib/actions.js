3f07f8f3b5487d79f808f358f87394cb25852d99
delroscol98 <collindelrosario98@gmail.com>
2024-11-13 20:47:19 +1100

    Updating the profile using a server action and manual cache revalidation

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,25 @@
 "use server";
 
-import { signIn, signOut } from "./auth";
+import { revalidatePath } from "next/cache";
+import { auth, signIn, signOut } from "./auth";
+import { updateGuest as updateGuestApi } from "./data-service";
+
+export async function updateGuest(formData) {
+  const session = await auth();
+  if (!session) throw new Error("You must be logged in");
+
+  const nationalID = formData.get("nationalID");
+  const [nationality, countryFlag] = formData.get("nationality").split("%");
+
+  if (!/^[a-zA-Z0-9]{6,12}$/.test(nationalID)) {
+    throw new Error("Please provide a valid national ID");
+  }
+
+  const updateData = { nationality, countryFlag, nationalID };
+  updateGuestApi(session.user.guestId, updateData);
+
+  revalidatePath("/account/profile");
+}
 
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
